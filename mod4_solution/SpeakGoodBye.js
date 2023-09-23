856d49bf3e4031bc61888ef8528adac8320d402d
nat45667 <143217836+nat45667@users.noreply.github.com>
2023-09-23 11:39:52 +0300

    Update SpeakGoodBye.js

--- mod4_solution/SpeakGoodBye.js
+++ mod4_solution/SpeakGoodBye.js
@@ -22,5 +22,4 @@ byeSpeaker.speak = function (name) {
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
-window.byeSpeaker = byeSpeaker;
-})(window);
+window.byeSpeaker = byeSpeaker;})(window);
